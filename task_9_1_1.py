31309f97fd3a21765dee59f87689c1346f7ded31
rockox1992 <94861200+rockox1992@users.noreply.github.com>
2023-06-22 20:12:09 +0300

    Update task_9_1_1.py
    print

--- task_9_1_1.py
+++ task_9_1_1.py
@@ -8,26 +8,33 @@
 """
 from math import sqrt
 from task_9_1_2 import read_csv
-from task_9_1_2 import deco
+from task_9_1_2 import deco_list, deco_json
 
-@deco
+
+
+@deco_json
+@deco_list
 def quadratic(*args):
     for ar in args:
         a = int(ar[0])
         b = int(ar[1])
         c = int(ar[2])
-        print(a, b, c)
+        #print(a, b, c)
         d = b ** 2 - 4 * a * c
+        #print(d)
         if d < 0:
-            return f'No root'
+            #return f'No root' с retern не работает, не понятно почему
+            print(f'No root')
         elif d == 0:
             x_1 = (- b) / (2 * a)
-            return x_1
+            print(x_1)
+            #return x_1 с retern не работает, не понятно почему, через принт работает.
         elif d > 0:
             x_1 = (- b + sqrt(d)) / (2 * a)
             x_2 = (- b - sqrt(d)) / (2 * a)
-            return x_1, x_2
+            print(x_1, x_2)
+            #return x_1, x_2 с retern не работает, не понятно почему
 
 
-print(quadratic(()))
+quadratic()
 
